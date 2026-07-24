073312c2f6346b2b1e58830be6006fee4e9c07b3
eesha12345 <eesha24mansoor@gmail.com>
2026-07-24 23:22:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ elif page == "CSV Upload":
 
     if uploaded_file is not None:
 
-        import ZipFile
+        import zipfile
         data = pd.read_csv(zipfile.Zipfile(uploaded_file).open("dataset.csv"))
 
         st.write("Uploaded Data")
